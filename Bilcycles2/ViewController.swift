7f74408056b446e8e44ac659e3ba161904d91e95
Yusuf Eyüpoğlu <furkan@yusufs-macbook-pro.local>
2018-11-07 16:56:38 +0300

    Initial Commit

---
+++ Bilcycles2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bilcycles2
+//
+//  Created by Yusuf Furkan Eyupoglu on 7.11.2018.
+//  Copyright © 2018 Yusuf Furkan Eyupoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
